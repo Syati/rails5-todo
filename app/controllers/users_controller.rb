4426f600bd13b7a9694a7414e5c15a2393b0d727
mizuki-y <mizuki-y@syati.info>
2016-09-25 23:50:54 +0900

    Add flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the rails5-todo"
       redirect_to @user
     else
       render 'new'
