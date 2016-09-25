e7d177f73840e5aaccb7600b93cd03fab7c06cfb
mizuki-y <mizuki-y@syati.info>
2016-09-25 22:01:23 +0900

    Add signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'static_pages#home'
-  get 'about', to: 'static_pages#about'
+  root           to: 'static_pages#home'
+  get 'about',   to: 'static_pages#about'
   get 'company', to: 'static_pages#company'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'signup',  to: 'users#new'
+  resources :users
 end
