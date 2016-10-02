d47225604b4cb0d457991a17da1d4259e103915b
mizuki-y <mizuki-y@syati.info>
2016-10-02 14:38:21 +0900

    Add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root           to: 'static_pages#home'
-  get 'about',   to: 'static_pages#about'
-  get 'company', to: 'static_pages#company'
-  get 'signup',  to: 'users#new'
+  root              to: 'static_pages#home'
+  get    'about',   to: 'static_pages#about'
+  get    'company', to: 'static_pages#company'
+  get    'signup',  to: 'users#new'
+  get    'login',   to: 'sessions#new'
+  post   'login',   to: 'sessions#create'
+  delete 'logout',  to: 'sessions#destroy'
+
   resources :users
 end
