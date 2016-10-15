b514c1dfec6d982a9cd745363fbd182e78dbc90c
mizuki-y <mizuki-y@syati.info>
2016-10-15 23:11:02 +0900

    Add friendly forwarding url

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,14 +1,4 @@
 module SessionsHelper
-  def log_in(user)
-    session[:user_id] = user.id
-  end
-
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -21,8 +11,8 @@ module SessionsHelper
     end
   end
 
-  def logged_in?
-    !current_user.nil?
+  def current_user?(user)
+    user == current_user
   end
 
   def forget(user)
@@ -31,9 +21,32 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
 
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
