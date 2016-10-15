46a8bcdcc89dec22d4574c13147a57283e572ce3
mizuki-y <mizuki-y@syati.info>
2016-10-15 18:50:40 +0900

    Use react-rails

---
+++ app/assets/javascripts/components/Header.js.jsx
@@ -0,0 +1,64 @@
+var Header = React.createClass({
+  propTypes: {
+    isLoggedIn: React.PropTypes.bool.isRequired,
+    user: React.PropTypes.Object
+  },
+  
+  getInitialState: function() {
+    return {
+      isLoggedIn: this.props.isLoggedIn,
+      showMenu: false,
+    };
+  },
+  
+  renderBeforeLogin: function() {
+    return (
+      <ul className="m-header-menu">
+        <li className="m-header-menu-item"><a href='/signup'>Sign up</a></li>
+        <li className="m-header-menu-item"><a href='/login'>Log in</a></li>
+      </ul>
+    );
+  },
+
+  handleClickSettings: function() {
+    this.setState({
+      showMenu: !this.state.showMenu,
+    });
+  },
+  
+  handleMouseLeaveFromSettings: function() {
+    this.setState({
+      showMenu: false,
+    });
+  },
+  
+  renderAfterLogin: function() {
+    return (
+      <ul className="m-header-menu">
+        <li className="m-header-menu-item" onMouseLeave={ this.handleMouseLeaveFromSettings }>
+          <a onClick={ this.handleClickSettings }>Settings</a>
+          <ul className={ this.state.showMenu ? '' : 'hidden' }>
+            <li>hoge</li>
+            <li>hoge</li>
+            <li><a href="/profile">Profile</a></li>
+            <li><a rel="nofollow" data-method="delete" href="/logout">Log out</a></li>            
+          </ul>
+        </li>
+      </ul>
+    );
+  },
+  
+  render: function() {
+    var component
+    if (this.state.isLoggedIn) {
+      component = this.renderAfterLogin();
+    } else {
+      component = this.renderBeforeLogin();
+    }
+    return (
+      <div className="m-header-nav-inner">
+        {component}
+      </div>
+    );
+  },
+});
