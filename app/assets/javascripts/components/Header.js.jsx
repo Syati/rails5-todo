e87c8df98531d1a03619ba8b37f5b9a4f2f3deb4
mizuki-y <mizuki-y@syati.info>
2016-10-16 16:05:48 +0900

    Add users index

--- app/assets/javascripts/components/Header.js.jsx
+++ app/assets/javascripts/components/Header.js.jsx
@@ -3,14 +3,14 @@ var Header = React.createClass({
     isLoggedIn: React.PropTypes.bool.isRequired,
     user: React.PropTypes.Object
   },
-  
+
   getInitialState: function() {
     return {
       isLoggedIn: this.props.isLoggedIn,
       showMenu: false,
     };
   },
-  
+
   renderBeforeLogin: function() {
     return (
       <ul className="m-header-menu">
@@ -25,31 +25,31 @@ var Header = React.createClass({
       showMenu: !this.state.showMenu,
     });
   },
-  
+
   handleMouseLeaveFromSettings: function() {
     this.setState({
       showMenu: false,
     });
   },
-  
+
   renderAfterLogin: function() {
     return (
       <ul className="m-header-menu">
         <li className="m-header-menu-item" onMouseLeave={ this.handleMouseLeaveFromSettings }>
           <a onClick={ this.handleClickSettings }>Settings</a>
           <ul className={ this.state.showMenu ? '' : 'hidden' }>
-            <li>hoge</li>
             <li><a href={'/users/' + this.props.user.id + '/edit'}>Edit</a></li>
             <li><a href={'/users/' + this.props.user.id}>Profile</a></li>
-            <li><a rel="nofollow" data-method="delete" href="/logout">Log out</a></li>            
+            <li><a rel="nofollow" data-method="delete" href="/logout">Log out</a></li>
           </ul>
         </li>
+        <li className="m-header-menu-item"><a href='/users/'>Users</a></li>
       </ul>
     );
   },
-  
+
   render: function() {
-    var component
+    var component;
     if (this.state.isLoggedIn) {
       component = this.renderAfterLogin();
     } else {
